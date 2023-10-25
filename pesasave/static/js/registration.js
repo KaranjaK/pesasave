d2be45972cdc473b4c639403f6da37c738227e7b
KaranjaK <karanjakelvin77@gmail.com>
2023-10-25 12:47:56 +0300

    ⚡️ Email validation messages

--- pesasave/static/js/registration.js
+++ pesasave/static/js/registration.js
@@ -1,8 +1,11 @@
 const usernameField = document.querySelector("#usernameField");
 const feedBackArea = document.querySelector(".invalid_feedback");
+const emailField = document.querySelector("#emailField");
+const emailFeedBackArea = document.querySelector(".emailFeedBackArea");
 const usernameSuccessOutput = document.querySelector(".usernameSuccessOutput");
 const submitBtn = document.querySelector(".submit-btn");
 
+// Username validation
 usernameField.addEventListener("keyup", (e) => {
     const usernameVal = e.target.value;
   
@@ -18,7 +21,7 @@ usernameField.addEventListener("keyup", (e) => {
     
   
     if (usernameVal.length > 0) {
-      fetch("/authentication/validate-username", {
+      fetch("/authentication/validate-username/", {
         body: JSON.stringify({ username: usernameVal }),
         method: "POST",
       })
@@ -30,6 +33,33 @@ usernameField.addEventListener("keyup", (e) => {
             feedBackArea.style.display = "block";
             feedBackArea.innerHTML = `<p>${data.username_error}</p>`;
             submitBtn.disabled = true;
+          } else {
+            submitBtn.removeAttribute("disabled");
+          }
+        });
+    }
+  });
+
+  // Email validation
+  emailField.addEventListener("keyup", (e) => {
+    const emailVal = e.target.value;
+  
+    emailField.classList.remove("is-invalid");
+    emailFeedBackArea.style.display = "none";
+  
+    if (emailVal.length > 0) {
+      fetch("/authentication/validate-email/", {
+        body: JSON.stringify({ email: emailVal }),
+        method: "POST",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log("data", data);
+          if (data.email_error) {
+            submitBtn.disabled = true;
+            emailField.classList.add("is-invalid");
+            emailFeedBackArea.style.display = "block";
+            emailFeedBackArea.innerHTML = `<p>${data.email_error}</p>`;
           } else {
             submitBtn.removeAttribute("disabled");
           }
