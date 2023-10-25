2f2ca57769e3f068a12362f9211088b048888e79
KaranjaK <karanjakelvin77@gmail.com>
2023-10-25 08:04:12 +0300

    📦 Email validation view

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,7 +3,8 @@ from . import views
 from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
-    path('register', views.RegistrationView.as_view(), name="register"),
-    path('validate-username', csrf_exempt(views.UsernameValidationView.as_view()),
+    path('register/', views.RegistrationView.as_view(), name="register"),
+    path('validate-username/', csrf_exempt(views.UsernameValidationView.as_view()),
          name="validate-username"),
+    path('validate-email/', csrf_exempt(views.EmailValidationView.as_view()), name="validate-email"),
 ]
