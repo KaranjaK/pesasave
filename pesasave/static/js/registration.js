6c5256337a3f380f813ced8ffe32796714f70ad7
KaranjaK <karanjakelvin77@gmail.com>
2023-10-25 21:39:48 +0300

    📦 It creates the messages template

--- pesasave/static/js/registration.js
+++ pesasave/static/js/registration.js
@@ -4,65 +4,79 @@ const emailField = document.querySelector("#emailField");
 const emailFeedBackArea = document.querySelector(".emailFeedBackArea");
 const usernameSuccessOutput = document.querySelector(".usernameSuccessOutput");
 const submitBtn = document.querySelector(".submit-btn");
+const passwordField = document.querySelector("#passwordField");
+const showPasswordToggle = document.querySelector(".showPasswordToggle");
 
 // Username validation
 usernameField.addEventListener("keyup", (e) => {
-    const usernameVal = e.target.value;
-  
-    usernameSuccessOutput.style.display = "block";
-  
-    usernameSuccessOutput.innerHTML = `<i class="bi bi-check2-all"></i> ${usernameVal}`;
-    setTimeout((e) =>{
-        usernameSuccessOutput.style.display = "none";
-    }, 5000)
-  
-    usernameField.classList.remove("is-invalid");
-    feedBackArea.style.display = "none";
-    
-  
-    if (usernameVal.length > 0) {
-      fetch("/authentication/validate-username/", {
-        body: JSON.stringify({ username: usernameVal }),
-        method: "POST",
-      })
-        .then((res) => res.json())
-        .then((data) => {
-          if (data.username_error) {
-            usernameSuccessOutput.style.display = "none";
-            usernameField.classList.add("is-invalid");
-            feedBackArea.style.display = "block";
-            feedBackArea.innerHTML = `<p>${data.username_error}</p>`;
-            submitBtn.disabled = true;
-          } else {
-            submitBtn.removeAttribute("disabled");
-          }
-        });
-    }
-  });
+  const usernameVal = e.target.value;
 
-  // Email validation
-  emailField.addEventListener("keyup", (e) => {
-    const emailVal = e.target.value;
-  
-    emailField.classList.remove("is-invalid");
-    emailFeedBackArea.style.display = "none";
-  
-    if (emailVal.length > 0) {
-      fetch("/authentication/validate-email/", {
-        body: JSON.stringify({ email: emailVal }),
-        method: "POST",
-      })
-        .then((res) => res.json())
-        .then((data) => {
-          console.log("data", data);
-          if (data.email_error) {
-            submitBtn.disabled = true;
-            emailField.classList.add("is-invalid");
-            emailFeedBackArea.style.display = "block";
-            emailFeedBackArea.innerHTML = `<p>${data.email_error}</p>`;
-          } else {
-            submitBtn.removeAttribute("disabled");
-          }
-        });
-    }
-  });
+  usernameSuccessOutput.style.display = "block";
+
+  usernameSuccessOutput.innerHTML = `<i class="bi bi-check2-all"></i> ${usernameVal}`;
+  setTimeout((e) => {
+    usernameSuccessOutput.style.display = "none";
+  }, 5000);
+
+  usernameField.classList.remove("is-invalid");
+  feedBackArea.style.display = "none";
+
+  if (usernameVal.length > 0) {
+    fetch("/authentication/validate-username/", {
+      body: JSON.stringify({ username: usernameVal }),
+      method: "POST",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.username_error) {
+          usernameSuccessOutput.style.display = "none";
+          usernameField.classList.add("is-invalid");
+          feedBackArea.style.display = "block";
+          feedBackArea.innerHTML = `<p>${data.username_error}</p>`;
+          submitBtn.disabled = true;
+        } else {
+          submitBtn.removeAttribute("disabled");
+        }
+      });
+  }
+});
+
+// Email validation
+emailField.addEventListener("keyup", (e) => {
+  const emailVal = e.target.value;
+
+  emailField.classList.remove("is-invalid");
+  emailFeedBackArea.style.display = "none";
+
+  if (emailVal.length > 0) {
+    fetch("/authentication/validate-email/", {
+      body: JSON.stringify({ email: emailVal }),
+      method: "POST",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("data", data);
+        if (data.email_error) {
+          submitBtn.disabled = true;
+          emailField.classList.add("is-invalid");
+          emailFeedBackArea.style.display = "block";
+          emailFeedBackArea.innerHTML = `<p>${data.email_error}</p>`;
+        } else {
+          submitBtn.removeAttribute("disabled");
+        }
+      });
+  }
+});
+
+// Password Toggle
+const handleToggleInput = (e) => {
+  if (showPasswordToggle.textContent === "SHOW") {
+    showPasswordToggle.textContent = "HIDE";
+    passwordField.setAttribute("type", "text");
+  } else {
+    showPasswordToggle.textContent = "SHOW";
+    passwordField.setAttribute("type", "password");
+  }
+};
+
+showPasswordToggle.addEventListener("click", handleToggleInput);
